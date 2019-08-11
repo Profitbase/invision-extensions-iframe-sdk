7fc90b70bffd1c6c1c6abac1530570be06cee813
Tore Senneseth <tore.senneseth@outlook.com>
2019-08-11 21:06:29 +0200

    Added option to set allowed target origins prevent sending messages to unwanted listeners

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,75 @@
 
-export default class invision {
-    public static sendMessage(message: any) {
-        const targetInfo = (window as any).invision_ext_iframe_api;
+const allowTargetOrigin = (targetOrigin : string, allowedTargetOrigins : string[]) : boolean => {
+    if(allowedTargetOrigins.length){
+        return allowedTargetOrigins.some(c => c.toLowerCase() === targetOrigin.toLowerCase());        
+    }
+    return true;
+}
 
-        if(!targetInfo){
-            console.error('Not hosted inside an InVision iFrame or communication is not initialized.');
+const setup = (evt : MessageEvent) => {
+    if (evt.data.source === 'pb_invision' && evt.data.type === 'com_config') {        
+        invision.CommunicationInfo = {            
+            messageAddress: evt.data.messageAddress,
+            targetOrigin: evt.data.targetOrigin,
+            origin: evt.data.origin,
+            sourceWindow: evt.source,
+            ...invision.CommunicationInfo,                       
+        };
+        
+        window.removeEventListener('message', setup);        
+    }
+}
+
+window.addEventListener('message', setup);
+
+type ChannelInfo = {
+    messageAddress: string,
+    targetOrigin: string,
+    origin: string,
+    sourceWindow: Window | MessagePort | ServiceWorker | null,
+    allowedTargetOrigins?: string[]
+};
+
+export type SendMessageConfig = {
+    message : any,
+    allowedTargetOrigins? : string[]
+}
+
+export default class invision {
+
+    public static CommunicationInfo : ChannelInfo;
+
+    public static sendMessage(message: SendMessageConfig | any) {
+        if(!message){
+            return;
+        }
+
+        const communicationInfo = invision.CommunicationInfo;
+        if (!communicationInfo || !communicationInfo.targetOrigin) {
+            console.error('Not hosted inside an InVision iFrame, communication is not initialized or now allowed (based on target origin restrictions).');
+            return;
+        }
+
+        const allowedTargetOrigins = [...communicationInfo.allowedTargetOrigins || [], ...message.allowedTargetOrigins || []];
+        if(!allowTargetOrigin(communicationInfo.targetOrigin, allowedTargetOrigins)){
             return;
         }
 
         window.parent.postMessage({
-            messageAddress: targetInfo.messageAddress,
-            message: message
-        }, targetInfo.targetOrigin);
+            messageAddress: communicationInfo.messageAddress,
+            message: message.message || message
+        }, communicationInfo.targetOrigin);
+    }
+
+    /**
+     * Sets the target origins which are allowed to send messages to using the sendMesage() function.
+     * You can also specify the allowed target origins in the message object passed to the sendMessage() function.
+     * If you send any type of sensitive information from your iframe extension to InVision using this API, you should specify the allowed target origins
+     * to ensure that no message is sent to unwanted listeners.
+     * @param allowedTargetOrigins 
+     */
+    public static setAllowedTargetOrigins(allowedTargetOrigins : string[]){
+        invision.CommunicationInfo = {...invision.CommunicationInfo, allowedTargetOrigins: allowedTargetOrigins};        
     }
 }
 
-window.addEventListener('message', function (evt: MessageEvent) {
-    if (evt.data.source === 'pb_invision' && evt.data.type === 'com_config') {
-        (window as any).invision_ext_iframe_api = {
-            messageAddress: evt.data.messageAddress,
-            targetOrigin: evt.data.targetOrigin
-        };
-    }
-});
